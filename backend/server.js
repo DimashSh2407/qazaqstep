53bff4a75176e884ea530817c65ba6a2c696bfbc
Dimash <5902257502@my.lerna.kz>
2026-02-01 21:27:23 +0500

    changed path

--- backend/server.js
+++ backend/server.js
@@ -10,8 +10,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
-
+app.use(express.static(path.join(__dirname, '..', 'public')));
 // MongoDB connection
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/qazaqstep';
 
